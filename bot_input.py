41085077e7c7b3dd5866d8158a15d8441081dde5
Poček Uroš <46105849+UPocek@users.noreply.github.com>
2022-03-03 23:21:34 +0100

    Add files via upload

--- bot_input.py
+++ bot_input.py
@@ -9,12 +9,12 @@ def not_occupied(tile, player):
         return not (t.x == tile[0] and t.y == tile[1])
 
 
-def what_is_near(tile, me, other):
+def what_is_near(tile, no, me, other):
     ok_tiles = []
     directions = [(1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1)]
     for d in directions:
         new = [tile[0] + d[0], tile[1] + d[1]]
-        if 0 <= new[0] <= 7 and 0 <= new[1] <= 7 and not_occupied(new, me) and not_occupied(new, other):
+        if 0 <= new[0] <= 7 and 0 <= new[1] <= 7 and not_occupied(new, me) and not_occupied(new, other) and new not in no:
             ok_tiles.append(new)
     return ok_tiles
 
@@ -23,10 +23,21 @@ class API:
     def __init__(self):
         self.dto = None
         self.matrix = [[None for i in range(8)] for j in range(8)]
+        self.my_tiles = None
         self.num_special = 0
         self.num_tiles = 0
-        self.tiles_checked = 0
+        self.tiles_available = None
+        self.now_both = None
         self.predicted_tiles = 0
+
+    def _what_is_mine(self):
+        my_tiles = []
+        for mt in self.dto.source.tiles:
+            my_tiles.append((mt.x, mt.y))
+        self.my_tiles = my_tiles
+        self.num_tiles = len(self.my_tiles)
+        self.tiles_available = my_tiles
+        self.now_bought = []
 
     def _create_matrix(self):
         for title in self.dto.tiles:
@@ -35,20 +46,36 @@ class API:
 
     def buy_best(self):
         available_to_buy = []
-        my_tiles = []
-        for mt in self.dto.source.tiles:
-            my_tiles.append((mt.x, mt.y))
-        for t in my_tiles:
-            available_to_buy.extend(what_is_near(t, self.dto.source, self.dto.enemy))
+        diagonal = []
+        for t in self.my_tiles:
+            available_to_buy.extend(what_is_near(t, self.now_bought, self.dto.source, self.dto.enemy))
         for tile in self.dto.tiles:
             if tile.bIsSpecial:
                 for a in available_to_buy:
                     if tile.x == a[0] and tile.y == a[1]:
                         return a
-        if len(available_to_buy) > 0:
-            self.land(available_to_buy[0])
+        for a in available_to_buy:
+            if a[0] == a[1]:
+                diagonal.append(a)
+        if len(diagonal) > 0:
+            return diagonal[0]
+        elif len(available_to_buy) > 0:
+            return available_to_buy[0]
         else:
             print("Nije mogao da pronadje ni jedan validan potez")
+            # SAMOTEST
+            exit()
+
+    def plant_on_best(self):
+        for tile in self.dto.tiles:
+            if tile.bIsSpeciala:
+                best = [tile.x, tile.y]
+                if best in self.tiles_available:
+                    self.tiles_available.remove(best)
+                    return best
+        best = self.tiles_available[0]
+        self.tiles_available.remove(best)
+        return best
 
     def water(self, list):
         actions = []
@@ -111,6 +138,7 @@ def bot_input(dto):
     global num_of_turn
     api.dto = dto
     api._create_matrix()
+    api._what_is_mine()
     num_of_turn += 1
     if num_of_turn == 1:
         return api.shop([(0, 1), (6, 1)])
@@ -128,7 +156,9 @@ def bot_input(dto):
         return api.water([(1, 0, 0)])
     elif num_of_turn == 8:
         return api.harvest()
-    elif num_of_turn == 8:
-        return api.land()
+    elif num_of_turn == 9:
+        return api.buy_best()
+    elif num_of_turn == 10:
+        return api.plant(())
 
     return "{}"
